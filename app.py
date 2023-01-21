a19f29d35451df11fc612330e4cf3a85608caaef
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2023-01-21 14:38:02 +0000

    CREATE: Created base.html, index.html and view.html, app.py and a basic styles.css

---
+++ app.py
@@ -0,0 +1,22 @@
+# import flask modules
+from flask import Flask, redirect, render_template, request, url_for
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secretkeycs3305"
+
+
+@app.route("/")     
+def index():
+    return render_template("index.html")
+  
+
+@app.route("/view", methods=["GET", "POST"])
+def view():
+    return render_template("view.html")
+
+
+
+if __name__ == "__main__":
+    app.run()
+ 
