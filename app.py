d1ed223fc847fe0833ee9fa3bbd26ff3424c3638
NathanielDev <121110414@umail.ucc.ie>
2023-01-25 15:18:45 +0000

    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "secretkeycs3305"
 
 
-@app.route("/")     
+@app.route("/")
 def index():
     return render_template("index.html")
 
@@ -14,7 +14,6 @@ def index():
 @app.route("/game", methods=["GET", "POST"])
 def game():
     return render_template("game.html")
-
 
 
 if __name__ == "__main__":
