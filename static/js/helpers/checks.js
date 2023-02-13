5262476eb60b20756b38d61659d32b3c7a11ab6e
= <=>
2023-02-13 23:42:34 +0000

    Added a function in static/js/helpers/checks.js for checking if a variable contains an instance of a given class

--- static/js/helpers/checks.js
+++ static/js/helpers/checks.js
@@ -9,3 +9,20 @@ export function checkIfNumber(x) {
         throw TypeError("Not an integer or float");
     }
 };
+
+export function checkIfInstance(x, c) {
+    if (c instanceof Object) {
+        const DESCRIPTORS = Object.getOwnPropertyDescriptors(c);
+
+        if (DESCRIPTORS.prototype === undefined || DESCRIPTORS.prototype.writable === undefined || DESCRIPTORS.prototype.writable === true) {
+            throw TypeError("Not a class");
+        }
+    }
+    else {
+        throw TypeError("Not a class");
+    }
+
+    if (x instanceof c === false) {
+        throw TypeError("Not an instance of " + c.name);
+    }
+};
