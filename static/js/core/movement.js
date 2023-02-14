d7d0d8bcba388587b35d21b8536b4cf15bd34bf0
= <=>
2023-02-14 10:34:31 +0000

    Added a function in static/js/core/movement.js that moves the player and checks if they have collided with an obstacle.

--- static/js/core/movement.js
+++ static/js/core/movement.js
@@ -14,3 +14,32 @@ export const MOVEMENT_KEY_STATUSES = {
     a: false,
     d: false
 };
+
+export function checkForCollisionsAndMovePlayer(sprite) {
+    checks.checkIfInstance(sprite, Player);
+
+    if (MOVEMENT_KEY_STATUSES.s && MOVEMENT_KEY_STATUSES.d && (checkCollisionWithTopEdgesOfObstacles(sprite) === false && checkCollisionWithLeftEdgesOfObstacles(sprite) === false)) {
+        sprite.moveSprite(sprite.movementOffset, sprite.movementOffset);
+    }
+    else if (MOVEMENT_KEY_STATUSES.s && MOVEMENT_KEY_STATUSES.a && (checkCollisionWithTopEdgesOfObstacles(sprite) === false && checkCollisionWithRightEdgesOfObstacles(sprite) === false)) {
+        sprite.moveSprite(-sprite.movementOffset, sprite.movementOffset);
+    }
+    else if (MOVEMENT_KEY_STATUSES.w && MOVEMENT_KEY_STATUSES.d  && (checkCollisionWithBottomEdgesOfObstacles(sprite) === false && checkCollisionWithLeftEdgesOfObstacles(sprite) === false)) {
+        sprite.moveSprite(sprite.movementOffset, -sprite.movementOffset);
+    }
+    else if (MOVEMENT_KEY_STATUSES.w && MOVEMENT_KEY_STATUSES.a && (checkCollisionWithBottomEdgesOfObstacles(sprite) === false && checkCollisionWithRightEdgesOfObstacles(sprite) === false)) {
+        sprite.moveSprite(-sprite.movementOffset, -sprite.movementOffset);
+    }
+    else if (MOVEMENT_KEY_STATUSES.w && checkCollisionWithBottomEdgesOfObstacles(sprite) === false) {
+        sprite.moveSprite(0, -sprite.movementOffset);
+    }
+    else if (MOVEMENT_KEY_STATUSES.s && checkCollisionWithTopEdgesOfObstacles(sprite) === false) {
+        sprite.moveSprite(0, sprite.movementOffset);
+    }
+    else if (MOVEMENT_KEY_STATUSES.a && checkCollisionWithRightEdgesOfObstacles(sprite) === false) {
+        sprite.moveSprite(-sprite.movementOffset, 0);
+    }
+    else if (MOVEMENT_KEY_STATUSES.d && checkCollisionWithLeftEdgesOfObstacles(sprite) === false) {
+        sprite.moveSprite(sprite.movementOffset, 0);
+    }
+};
