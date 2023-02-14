7d2534bd34cd89a26e4c1deacb218e5f59083391
= <=>
2023-02-14 09:04:58 +0000

    Deleted setSpriteFrameDimensions from the Sprite class in static/js/sprites/base/base.js

--- static/js/sprites/base/base.js
+++ static/js/sprites/base/base.js
@@ -121,12 +121,4 @@ export class Sprite {
         this.spriteFrameWidth = frameWidth;
         this.spriteFrameHeight = frameHeight;
     };
-
-    setSpriteFrameDimensions(w, h) {
-        checks.checkIfNumber(w);
-        checks.checkIfNumber(h);
-
-        this.spriteFrameWidth = w;
-        this.spriteFrameHeight = h;
-    };
 };
