f5f01b0973bcdf136cb15f24cd7568fa7ba13633
= <=>
2023-02-14 09:47:15 +0000

    Added a subclass of Objects in static/js/sprites/obstacles.js called 'Obstacle'

--- static/js/sprites/objects.js
+++ static/js/sprites/objects.js
@@ -1,8 +1,17 @@
 import * as checks from '../helpers/checks.js';
 import { Sprite } from './base/base.js';
+import { OBSTACLES } from '../core/collision.js';
 
 export class Objects extends Sprite {
     constructor(texture, posX, posY, frameWidth, frameHeight) {
         super(texture, posX, posY, frameWidth, frameHeight);
+    };
+};
+
+export class Obstacle extends Objects {
+    constructor(texture, posX, posY, frameWidth, frameHeight) {
+        super(texture, posX, posY, frameWidth, frameHeight);
+
+        OBSTACLES.push(this);
     };
 };
