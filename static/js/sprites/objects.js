29c8ff436075990309271f342a52964d55616914
= <=>
2023-02-14 09:43:59 +0000

    Imported Sprite class from static/js/sprites/base/base.js to static/js/sprites/objects.js and added a class called 'Objects' that extends it.

--- static/js/sprites/objects.js
+++ static/js/sprites/objects.js
@@ -1,1 +1,8 @@
 import * as checks from '../helpers/checks.js';
+import { Sprite } from './base/base.js';
+
+export class Objects extends Sprite {
+    constructor(texture, posX, posY, frameWidth, frameHeight) {
+        super(texture, posX, posY, frameWidth, frameHeight);
+    };
+};
