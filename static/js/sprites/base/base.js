b8b71f6f5f1dd82c1c858396160d53dc399cc8c3
= <=>
2023-02-14 08:48:50 +0000

    Added a class called 'Sprite' to static/js/sprites/base/base.js and created a constructor for it with all the attributes it requires.

--- static/js/sprites/base/base.js
+++ static/js/sprites/base/base.js
@@ -4,3 +4,31 @@ import {
     OBSTACLES,
     NON_PLAYER_ENTITIES
 } from './collision.js';
+
+
+
+export class Sprite {
+    constructor(texture, posX, posY, frameWidth, frameHeight) {
+        checks.checkIfInstance(texture, PIXI.Texture);
+        checks.checkIfNumber(posX);
+        checks.checkIfNumber(posY);
+        checks.checkIfNumber(frameWidth);
+        checks.checkIfNumber(frameHeight);
+
+        this.sprite = new PIXI.Sprite(texture);
+        this.spriteFrameWidth = frameWidth;
+        this.spriteFrameHeight = frameHeight;
+
+        this.sprite_container = new PIXI.Container();
+        this.sprite_container.addChild(this.sprite);
+        this.sprite_container.x = posX;
+        this.sprite_container.y = posY;
+
+        this.frameMask = null;
+        this.frames = {};
+        this.currentFrame = null;
+
+        this.isFlippedHorizontally = false;
+        this.isFlippedVertically = false;
+    };
+};
