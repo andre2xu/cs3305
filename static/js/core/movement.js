65f98296556cfbc55c4fdc367aa3c71ac073162f
= <=>
2023-02-14 10:33:11 +0000

    Added an object in static/js/core/movement.js to keep track of which movement key is being held by the player.

--- static/js/core/movement.js
+++ static/js/core/movement.js
@@ -1,1 +1,16 @@
 import * as checks from '../helpers/checks.js';
+import { Player } from '../sprites/entities.js';
+
+import {
+    checkCollisionWithLeftEdgesOfObstacles,
+    checkCollisionWithRightEdgesOfObstacles,
+    checkCollisionWithTopEdgesOfObstacles,
+    checkCollisionWithBottomEdgesOfObstacles
+} from '../core/collision.js';
+
+export const MOVEMENT_KEY_STATUSES = {
+    w: false,
+    s: false,
+    a: false,
+    d: false
+};
