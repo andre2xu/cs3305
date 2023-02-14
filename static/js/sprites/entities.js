be27a19b4ca5fe6946bdf38c3ebc186623ccde6f
= <=>
2023-02-14 10:04:05 +0000

    Added a class called 'Entity' in static/js/sprites/entities.js

--- static/js/sprites/entities.js
+++ static/js/sprites/entities.js
@@ -1,3 +1,14 @@
 import * as checks from '../helpers/checks.js';
 import { Sprite } from './base/base.js';
 import { NON_PLAYER_ENTITIES } from '../core/collision.js';
+
+export class Entity extends Sprite {
+    constructor(texture, posX, posY, frameWidth, frameHeight) {
+        super(texture, posX, posY, frameWidth, frameHeight);
+
+        this.movementOffset = 5;
+        this.events = {
+            move: null
+        };
+    };
+};
