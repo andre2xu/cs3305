468602302c753706f11a0518054223acaebf9ac9
= <=>
2023-02-14 09:53:04 +0000

    Added a method to the Obstacle class in static/js/sprites/objects.js for checking if a sprite collided with its instance's bottom edge.

--- static/js/sprites/objects.js
+++ static/js/sprites/objects.js
@@ -83,6 +83,29 @@ export class Obstacle extends Objects {
 
         return true;
     };
+
+    checkIfBottomEdgeCollisionOccurred(sprite) {
+        checks.checkIfInstance(sprite, Sprite);
+
+        if (sprite.getRightPosX() < this.getLeftPosX()) {
+            // if the sprite's right edge is far from the object's left edge
+            return false;
+        }
+        else if (sprite.getLeftPosX() > this.getRightPosX()) {
+            // if the sprite's left edge is far from the object's right edge
+            return false;
+        }
+        else if (sprite.getLeftPosY() > this.getRightPosY()) {
+            // if the sprite's top edge is lower than the object's bottom edge
+            return false;
+        }
+        else if (sprite.getRightPosY() < this.getRightPosY()) {
+            // if the sprite's bottom edge is beyond the object's bottom edge
+            return false;
+        }
+
+        return true;
+    };
 };
 
 export class Decoration extends Objects {
