359942517d67935883240ca807fb2920c1f25452
= <=>
2023-02-15 15:25:51 +0000

    Changed the collision detection mechanism for right object edges. Also fixed the collision detection mechanism for the left.

--- static/js/core/collision.js
+++ static/js/core/collision.js
@@ -10,10 +10,11 @@ function checkCollisionWithObstacles(sprite, side) {
     const NUM_OF_OBSTACLES = OBSTACLES.length;
 
     if (NUM_OF_OBSTACLES > 0) {
-        const SLX = sprite.getLeftPosX();
-        const SLY = sprite.getLeftPosY();
-        const SRX = sprite.getRightPosX() + sprite.getSpeed();
-        const SRY = sprite.getRightPosY() + sprite.getSpeed();
+        const SPRITE_SPEED = sprite.getSpeed();
+        const SLX = sprite.getLeftPosX() - SPRITE_SPEED;
+        const SLY = sprite.getLeftPosY() - SPRITE_SPEED;
+        const SRX = sprite.getRightPosX() + SPRITE_SPEED;
+        const SRY = sprite.getRightPosY() + SPRITE_SPEED;
 
         for (let i=0; i < NUM_OF_OBSTACLES; i++) {
             const OBSTACLE = OBSTACLES[i];
@@ -44,14 +45,19 @@ function checkCollisionWithObstacles(sprite, side) {
                 }
             }
             else if (side === 'left') {
-                const RIGHT_X_IS_BEHIND = SRX < OLX;
+                const RIGHT_X_IS_BEHIND_LEFT_EDGE = SRX < OLX;
+                const RIGHT_X_IS_IN_FRONT_OF_LEFT_EDGE = SRX > OLX;
+                const RIGHT_X_IS_IN_FRONT_OF_RIGHT_EDGE = SRX > ORX;
                 const IN_BETWEEN = SLY >= OLY && SRY <= ORY;
                 const TOP_EDGE_IS_OUTSIDE = SLY < OLY;
                 const BOTTOM_EDGE_IS_OUTSIDE = SRY > ORY;
                 const TOP_EDGE_IS_INSIDE = SLY >= OLY && SLY <= ORY;
                 const BOTTOM_EDGE_IS_INSIDE = SRY >= OLY && SRY <= ORY;
 
-                if (RIGHT_X_IS_BEHIND === false) {
+                if (RIGHT_X_IS_IN_FRONT_OF_LEFT_EDGE && RIGHT_X_IS_IN_FRONT_OF_RIGHT_EDGE) {
+                    return false;
+                }
+                else if (RIGHT_X_IS_BEHIND_LEFT_EDGE === false) {
                     if (IN_BETWEEN) {
                         return true;
                     }
@@ -63,15 +69,34 @@ function checkCollisionWithObstacles(sprite, side) {
                     }
                 }
             }
+            else if (side === 'right') {
+                const LEFT_X_IS_IN_FRONT_OF_RIGHT_EDGE = SLX > ORX;
+                const LEFT_X_IS_BEHIND_LEFT_EDGE = SLX < OLX;
+                const LEFT_X_IS_BEHIND_RIGHT_EDGE = SLX < ORX;
+                const IN_BETWEEN = SLY >= OLY && SRY <= ORY;
+                const TOP_EDGE_IS_OUTSIDE = SLY < OLY;
+                const BOTTOM_EDGE_IS_OUTSIDE = SRY > ORY;
+                const TOP_EDGE_IS_INSIDE = SLY >= OLY && SLY <= ORY;
+                const BOTTOM_EDGE_IS_INSIDE = SRY >= OLY && SRY <= ORY;
 
-
-
-            // else if (side === 'right' && OBSTACLE.checkIfRightEdgeCollisionOccurred(sprite)) {
-            //     return true;
-            // }
-            // else if (side === 'bottom' && OBSTACLE.checkIfBottomEdgeCollisionOccurred(sprite)) {
-            //     return true;
-            // }
+                if (LEFT_X_IS_BEHIND_LEFT_EDGE && LEFT_X_IS_BEHIND_RIGHT_EDGE) {
+                    return false;
+                }
+                else if (LEFT_X_IS_IN_FRONT_OF_RIGHT_EDGE === false) {
+                    if (IN_BETWEEN) {
+                        return true;
+                    }
+                    else if (TOP_EDGE_IS_OUTSIDE && BOTTOM_EDGE_IS_INSIDE) {
+                        return true
+                    }
+                    else if (BOTTOM_EDGE_IS_OUTSIDE && TOP_EDGE_IS_INSIDE) {
+                        return true;
+                    }
+                }
+            }
+            else if (side === 'bottom') {
+                1
+            }
         }
     }
 
