e440500232b4f75031e41935ff19fb4b118e5674
= <=>
2023-02-15 17:08:50 +0000

    Added wall sliding movement.

--- static/js/core/movement.js
+++ static/js/core/movement.js
@@ -18,17 +18,61 @@ export const MOVEMENT_KEY_STATUSES = {
 export function checkForCollisionsAndMovePlayer(sprite) {
     checks.checkIfInstance(sprite, Player);
 
-    if (MOVEMENT_KEY_STATUSES.s && MOVEMENT_KEY_STATUSES.d && (checkCollisionWithTopEdgesOfObstacles(sprite) === false && checkCollisionWithLeftEdgesOfObstacles(sprite) === false)) {
-        sprite.moveSpriteSouthEast();
+    if (MOVEMENT_KEY_STATUSES.s && MOVEMENT_KEY_STATUSES.d) {
+        const COLLIDED_WITH_A_TOP_EDGE = checkCollisionWithTopEdgesOfObstacles(sprite);
+        const COLLIDED_WITH_A_LEFT_EDGE = checkCollisionWithLeftEdgesOfObstacles(sprite);
+
+        if (COLLIDED_WITH_A_TOP_EDGE === false && COLLIDED_WITH_A_LEFT_EDGE === false) {
+            sprite.moveSpriteSouthEast();
+        }
+        else if (COLLIDED_WITH_A_TOP_EDGE) {
+            sprite.moveSpriteEast();
+        }
+        else if (COLLIDED_WITH_A_LEFT_EDGE) {
+            sprite.moveSpriteSouth();
+        }
     }
-    else if (MOVEMENT_KEY_STATUSES.s && MOVEMENT_KEY_STATUSES.a && (checkCollisionWithTopEdgesOfObstacles(sprite) === false && checkCollisionWithRightEdgesOfObstacles(sprite) === false)) {
-        sprite.moveSpriteSouthWest();
+    else if (MOVEMENT_KEY_STATUSES.s && MOVEMENT_KEY_STATUSES.a) {
+        const COLLIDED_WITH_A_TOP_EDGE = checkCollisionWithTopEdgesOfObstacles(sprite);
+        const COLLIDED_WITH_A_RIGHT_EDGE = checkCollisionWithRightEdgesOfObstacles(sprite);
+
+        if (COLLIDED_WITH_A_TOP_EDGE === false && COLLIDED_WITH_A_RIGHT_EDGE === false) {
+            sprite.moveSpriteSouthWest();
+        }
+        else if (COLLIDED_WITH_A_TOP_EDGE) {
+            sprite.moveSpriteWest();
+        }
+        else if (COLLIDED_WITH_A_RIGHT_EDGE) {
+            sprite.moveSpriteSouth();
+        }
     }
-    else if (MOVEMENT_KEY_STATUSES.w && MOVEMENT_KEY_STATUSES.d && (checkCollisionWithBottomEdgesOfObstacles(sprite) === false && checkCollisionWithLeftEdgesOfObstacles(sprite) === false)) {
-        sprite.moveSpriteNorthEast();
+    else if (MOVEMENT_KEY_STATUSES.w && MOVEMENT_KEY_STATUSES.d) {
+        const COLLIDED_WITH_A_BOTTOM_EDGE = checkCollisionWithBottomEdgesOfObstacles(sprite);
+        const COLLIDED_WITH_A_LEFT_EDGE = checkCollisionWithLeftEdgesOfObstacles(sprite);
+
+        if (COLLIDED_WITH_A_BOTTOM_EDGE === false && COLLIDED_WITH_A_LEFT_EDGE === false) {
+            sprite.moveSpriteNorthEast();
+        }
+        else if (COLLIDED_WITH_A_BOTTOM_EDGE) {
+            sprite.moveSpriteEast();
+        }
+        else if (COLLIDED_WITH_A_LEFT_EDGE) {
+            sprite.moveSpriteNorth();
+        }
     }
-    else if (MOVEMENT_KEY_STATUSES.w && MOVEMENT_KEY_STATUSES.a && (checkCollisionWithBottomEdgesOfObstacles(sprite) === false && checkCollisionWithRightEdgesOfObstacles(sprite) === false)) {
-        sprite.moveSpriteNorthWest();
+    else if (MOVEMENT_KEY_STATUSES.w && MOVEMENT_KEY_STATUSES.a) {
+        const COLLIDED_WITH_A_BOTTOM_EDGE = checkCollisionWithBottomEdgesOfObstacles(sprite);
+        const COLLIDED_WITH_A_RIGHT_EDGE = checkCollisionWithRightEdgesOfObstacles(sprite);
+
+        if (COLLIDED_WITH_A_BOTTOM_EDGE === false && COLLIDED_WITH_A_RIGHT_EDGE === false) {
+            sprite.moveSpriteNorthWest();
+        }
+        else if (COLLIDED_WITH_A_BOTTOM_EDGE) {
+            sprite.moveSpriteWest();
+        }
+        else if (COLLIDED_WITH_A_RIGHT_EDGE) {
+            sprite.moveSpriteNorth();
+        }
     }
     else if (MOVEMENT_KEY_STATUSES.w && checkCollisionWithBottomEdgesOfObstacles(sprite) === false) {
         sprite.moveSpriteNorth();
