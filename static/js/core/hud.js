dbf20a8f5916fe29ffa0ced264d1a5dec1898737
= <=>
2023-02-20 18:58:29 +0000

    Changed position of player health status.

--- static/js/core/hud.js
+++ static/js/core/hud.js
@@ -4,6 +4,8 @@ export const PLAYER_HEALTH_STATUS = new PIXI.Text('Health: 100', {
     fontSize: 20,
     fill: 0xffffff,
 });
+PLAYER_HEALTH_STATUS.x = 20;
+PLAYER_HEALTH_STATUS.y = 10;
 
 export function updatePlayerHealthStatus(new_value) {
     checks.checkIfNumber(new_value);
