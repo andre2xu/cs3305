ef4f3114c366011ead800abf4f6b7c8cd069bf97
= <=>
2023-02-25 11:01:27 +0000

    Added a subclass for HealingItem called BandageBox.

--- static/js/sprites/consumables.js
+++ static/js/sprites/consumables.js
@@ -1,10 +1,19 @@
 import * as checks from '../helpers/checks.js';
-import { Item } from './base/base.js'; 
+import { Item } from './base/base.js';
+import { getTextureFromStaticAssetsFolder } from '../helpers/pixi_helpers.js';
 
 
 
 export class HealingItem extends Item {
     constructor(texture) {
         super(texture);
+    };
+};
+
+export class BandageBox extends HealingItem {
+    constructor() {
+        super(getTextureFromStaticAssetsFolder('/consumables/bandage_box.png'));
+
+        this.health = 20;
     };
 };
