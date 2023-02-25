1edacffaace395baaf1cb915eb9435e1495f01f9
= <=>
2023-02-25 16:42:09 +0000

    Improved readability and modified collision boundaries.

--- static/js/map/library/library.js
+++ static/js/map/library/library.js
@@ -1,12 +1,9 @@
-
 import { PlayableArea } from '../creation.js';
 import { getTextureFromStaticJSFolder } from '../../helpers/pixi_helpers.js';
 
 import {
     Decoration,
-    DecorationFill,
-    SemiSolid,
-    SemiSolidFill
+    SemiSolid
 } from '../../sprites/objects.js';
 
 export const LIBRARY = (function () {
@@ -16,9 +13,12 @@ export const LIBRARY = (function () {
     LIBRARY.addStaticSprite(
         FLOOR, 
         'libraryfloor', 
-        0, 0);
+        0,
+        0
+    );
 
-    
+
+
     const SECOND_FLOOR_MAT = new Decoration(getTextureFromStaticJSFolder('/map/library/assets/mat2.png'), 0, 0, 32, 34);
     LIBRARY.addStaticSprite(
         SECOND_FLOOR_MAT,
@@ -27,67 +27,97 @@ export const LIBRARY = (function () {
         20
     );
 
-    const BOOKSHELF = new SemiSolid(getTextureFromStaticJSFolder('/map/library/assets/bookshelf.png'), 0, 0, 59, 63);
-    LIBRARY.addStaticSprite(
-        BOOKSHELF,
-        'bookshelf',
-        LIBRARY.getWidth() - (BOOKSHELF.getSpriteFrameDimensions().w + 35),
-        LIBRARY.getHeight() - (BOOKSHELF.getSpriteFrameDimensions().h + 35)
+
+
+    const BOOKSHELF_1 = new SemiSolid(getTextureFromStaticJSFolder('/map/library/assets/bookshelf.png'), 0, 0, 95, 97);
+    BOOKSHELF_1.modifyCollisionBoundary(
+        null,
+        BOOKSHELF_1.getSpriteFrameDimensions().h - 30, null,
+        -20
     );
-    
-   
-    const BOOKSHELF2 = new SemiSolid(getTextureFromStaticJSFolder('/map/library/assets/bookshelf.png'), 0, 0, 59, 63);
     LIBRARY.addStaticSprite(
-        BOOKSHELF2,
+        BOOKSHELF_1,
+        'bookshelf1',
+        LIBRARY.getWidth() - BOOKSHELF_1.getSpriteFrameDimensions().w,
+        LIBRARY.getHeight() - (BOOKSHELF_1.getSpriteFrameDimensions().h + 35)
+    );
+
+
+
+    const BOOKSHELF_2 = new SemiSolid(getTextureFromStaticJSFolder('/map/library/assets/bookshelf.png'), 0, 0, 95, 97);
+    BOOKSHELF_2.modifyCollisionBoundary(
+        null,
+        BOOKSHELF_2.getSpriteFrameDimensions().h - 30, null,
+        -20
+    );
+    LIBRARY.addStaticSprite(
+        BOOKSHELF_2,
         'bookshelf2',
-        LIBRARY.getWidth() - (BOOKSHELF2.getSpriteFrameDimensions().w + 35),
-        LIBRARY.getHeight() - (BOOKSHELF2.getSpriteFrameDimensions().h + 170)
+        LIBRARY.getWidth() - BOOKSHELF_2.getSpriteFrameDimensions().w,
+        LIBRARY.getHeight() - (BOOKSHELF_2.getSpriteFrameDimensions().h + 150)
     );
-    
 
-    const CHAIRA = new SemiSolid(getTextureFromStaticJSFolder('/map/library/assets/chairA.png'), 0, 0, 59, 63);
+
+
+    const CHAIR_A = new SemiSolid(getTextureFromStaticJSFolder('/map/library/assets/chairA.png'), 0, 0, 75, 48);
     LIBRARY.addStaticSprite(
-        CHAIRA,
+        CHAIR_A,
         'chairA',
-        LIBRARY.getWidth() - (CHAIRA.getSpriteFrameDimensions().w + 435),
-        LIBRARY.getHeight() - (CHAIRA.getSpriteFrameDimensions().h + 5)
-    );
-   
-    const CHAIRB = new SemiSolid(getTextureFromStaticJSFolder('/map/library/assets/chairB.png'), 0, 0, 59, 63);
-    LIBRARY.addStaticSprite(
-        CHAIRB,
-        'chairB',
-        LIBRARY.getWidth() - (CHAIRB.getSpriteFrameDimensions().w + 350),
-        LIBRARY.getHeight() - (CHAIRB.getSpriteFrameDimensions().h + 82)
+        20,
+        LIBRARY.getHeight() - (CHAIR_A.getSpriteFrameDimensions().h + 15)
     );
 
-    const MATBLUE = new SemiSolid(getTextureFromStaticJSFolder('/map/library/assets/matblue.png'), 0, 0, 59, 63);
+
+
+    const CHAIR_B = new SemiSolid(getTextureFromStaticJSFolder('/map/library/assets/chairB.png'), 0, 0, 48, 75);
+    CHAIR_B.modifyCollisionBoundary(
+        null,
+        40,
+        null,
+        null
+    );
+    LIBRARY.addStaticSprite(
+        CHAIR_B,
+        'chairB',
+        130,
+        LIBRARY.getHeight() - (CHAIR_B.getSpriteFrameDimensions().h + 75)
+    );
+
+
+
+    const MATBLUE = new Decoration(getTextureFromStaticJSFolder('/map/library/assets/matblue.png'), 0, 0, 71, 54);
     LIBRARY.addStaticSprite(
         MATBLUE,
         'matblue',
-        LIBRARY.getWidth() - (MATBLUE.getSpriteFrameDimensions().w + 360),
-        LIBRARY.getHeight() - (MATBLUE.getSpriteFrameDimensions().h + 5)
+        120,
+        LIBRARY.getHeight() - (MATBLUE.getSpriteFrameDimensions().h + 12)
     );
 
-    const TABLELAMP = new SemiSolid(getTextureFromStaticJSFolder('/map/library/assets/tablelamp.png'), 0, 0, 59, 63);
+
+
+    const TABLELAMP = new SemiSolid(getTextureFromStaticJSFolder('/map/library/assets/tablelamp.png'), 0, 0, 95, 66);
+    TABLELAMP.modifyCollisionBoundary(
+        null,
+        30,
+        null,
+        null
+    );
     LIBRARY.addStaticSprite(
         TABLELAMP,
         'tablelamp',
-        LIBRARY.getWidth() - (TABLELAMP.getSpriteFrameDimensions().w + 455),
-        LIBRARY.getHeight() - (TABLELAMP.getSpriteFrameDimensions().h + 100)
+        10,
+        LIBRARY.getHeight() - (TABLELAMP.getSpriteFrameDimensions().h + 80)
     );
-    
 
-    const WINDOW = new SemiSolid(getTextureFromStaticJSFolder('/map/library/assets/window.png'), 0, 0, 59, 63);
+
+
+    const WINDOWS = new Decoration(getTextureFromStaticJSFolder('/map/library/assets/window.png'), 0, 0, 525, 75);
     LIBRARY.addStaticSprite(
-        WINDOW,
+        WINDOWS,
         'window',
-        0,
-        -(WINDOW.getSpriteFrameDimensions().h - 5)
+        -6,
+        -(WINDOWS.getSpriteFrameDimensions().h - 5)
     );
-
-
-
 
 
 
