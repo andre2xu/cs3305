a798107c934912f0bb71e47e319c79c46c697bc2
= <=>
2023-02-25 10:37:24 +0000

    Added a subclass of Item called HealingItem.

--- static/js/sprites/consumables.js
+++ static/js/sprites/consumables.js
@@ -1,2 +1,10 @@
 import * as checks from '../helpers/checks.js';
 import { Item } from './base/base.js'; 
+
+
+
+export class HealingItem extends Item {
+    constructor(texture) {
+        super(texture);
+    };
+};
