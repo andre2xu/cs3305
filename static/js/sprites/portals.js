dc0143dd507641324202151fde7a1aff8ba651e7
= <=>
2023-02-26 11:08:53 +0000

    Added a file called 'portals.js' for a class that represents sprites which move players to a different level.

---
+++ static/js/sprites/portals.js
@@ -0,0 +1,7 @@
+import { Decoration} from "./objects";
+
+export class Portal extends Decoration {
+    constructor(texture, posX, posY, frameWidth, frameHeight) {
+        super(texture, posX, posY, frameWidth, frameHeight);
+    };
+};
