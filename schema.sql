c3cbd40f9c0721df053a4775e8ae9991f40b1c3e
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2023-02-26 16:38:22 +0000

    Update app.db and schema.sql

--- schema.sql
+++ schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE users (
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
+    -- store time of game play in the database as an integer --
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
     );
