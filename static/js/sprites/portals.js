768836903531c740f5358129344d37f85343926f
= <=>
2023-02-26 11:28:11 +0000

    Added a setter for binding a playable area to a portal.

--- static/js/sprites/portals.js
+++ static/js/sprites/portals.js
@@ -1,9 +1,22 @@
+import * as checks from '../helpers/checks.js';
 import { Decoration } from "./objects.js";
+import { PlayableArea } from "../map/creation.js";
 
 export const PORTALS = [];
 
 export class Portal extends Decoration {
     constructor(texture, posX, posY, frameWidth, frameHeight) {
         super(texture, posX, posY, frameWidth, frameHeight);
+
+        this.destination = null;
+    };
+
+
+
+    // SETTERS
+    setDestination(playableArea) {
+        checks.checkIfInstance(playableArea, PlayableArea);
+
+        this.destination = playableArea;
     };
 };
