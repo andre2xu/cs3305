065251f3599dedb1410d3652953ebaac35b06980
= <=>
2023-02-26 11:17:23 +0000

    Added an array for portals.

--- static/js/sprites/portals.js
+++ static/js/sprites/portals.js
@@ -1,5 +1,7 @@
 import { Decoration } from "./objects.js";
 
+export const PORTALS = [];
+
 export class Portal extends Decoration {
     constructor(texture, posX, posY, frameWidth, frameHeight) {
         super(texture, posX, posY, frameWidth, frameHeight);
