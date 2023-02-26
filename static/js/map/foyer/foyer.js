0e634a6abded4984bbacdd244d9001d81d553e15
= <=>
2023-02-26 12:02:50 +0000

    Portals now require an origin (playable area) for initialization.

--- static/js/map/foyer/foyer.js
+++ static/js/map/foyer/foyer.js
@@ -83,7 +83,7 @@ export const FOYER = (function () {
 
 
 
-    const SECOND_FLOOR_MAT = new Portal(getTextureFromStaticJSFolder('/map/foyer/assets/mat2.png'), 0, 0, 32, 34);
+    const SECOND_FLOOR_MAT = new Portal(FOYER, getTextureFromStaticJSFolder('/map/foyer/assets/mat2.png'), 0, 0, 32, 34);
     SECOND_FLOOR_MAT.setDestination(LIBRARY, 0, 0);
     FOYER.addStaticSprite(
         SECOND_FLOOR_MAT,
