bd74c8b0849380f9c1023f5ac43b5ae4987d7e22
SharonHenning <72208574+SharonHenning@users.noreply.github.com>
2023-02-28 16:09:03 +0000

    Register player1 and update details on register form

--- forms.py
+++ forms.py
@@ -7,7 +7,7 @@ from wtforms.validators import InputRequired, EqualTo
 class RegistrationForm(FlaskForm):        
     username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired()])
-    password2 = PasswordField("Confirm Password", validators=[InputRequired(), EqualTo("password")])
+    password2 = PasswordField("Re-enter", validators=[InputRequired(), EqualTo("password")])
     register = SubmitField("Register")
 
 
