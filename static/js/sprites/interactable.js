1c1d4f7e0a56ba66de60c29f56c4dd1f3dcb5356
= <=>
2023-02-28 13:48:03 +0000

    Made the 'playerIsNearInteractable' method more readable.

--- static/js/sprites/interactable.js
+++ static/js/sprites/interactable.js
@@ -13,25 +13,23 @@ export function ammoCache(gun) {
 
 
 
-export class Interactable extends Objects{
+export const INTERACTABLES = [];
 
-    constructor(texture, posX, posY, frameWidth, frameHeight, price, isPaidOnlyOnce, onUse, player) {
+export class Interactable extends Objects{
+    constructor(texture, posX, posY, frameWidth, frameHeight, price, isPaidOnlyOnce, onUse) {
         super(texture, posX, posY, frameWidth, frameHeight);
 
         checks.checkIfNumber(price);
         checks.checkIfBoolean(isPaidOnlyOnce);
         checks.checkIfFunction(onUse);
-        checks.checkIfInstance(player, Player);
 
 
 
         this.price = price;
 
-        this.isPaidOnlyOnce = isPaidOnlyOnce; // flag to differ betweensuff that only needs to be paid once (doors) or stuff that needs to be repeatedly paid for
+        this.isPaidOnlyOnce = isPaidOnlyOnce; // flag to differ between interactables that only need to be paid once (e.g. doors), and those that need to be repeatedly paid for (e.g. ammo cache)
 
         this.onUse = onUse // function to be executed when user interacts
-
-        this.player = player;
 
         this.isActive = false;
 
@@ -41,10 +39,16 @@ export class Interactable extends Objects{
 
 
     // GETTERS
-    playerIsNearInteractable() {
+    playerIsNearInteractable(player) {
+        checks.checkIfInstance(player);
+
         const PLAYER_CENTER = this.player.getCenterCoordinates();
 
-        if (PLAYER_CENTER.x > this.getLeftPosX()-this.areaOfEffect && PLAYER_CENTER.x < this.getRightPosX()+this.areaOfEffect && PLAYER_CENTER.y > this.getLeftPosY()-this.areaOfEffect && PLAYER_CENTER.y < this.getRightPosY()+this.areaOfEffect) {
+        const PLAYER_INSIDE_LR_EDGES = PLAYER_CENTER.x > (this.getLeftPosX() - this.areaOfEffect) && PLAYER_CENTER.x < (this.getRightPosX() + this.areaOfEffect);
+
+        const PLAYER_INSIDE_TB_EDGES = PLAYER_CENTER.y > (this.getLeftPosY() - this.areaOfEffect) && PLAYER_CENTER.y < (this.getRightPosY() + this.areaOfEffect);
+
+        if (PLAYER_INSIDE_LR_EDGES && PLAYER_INSIDE_TB_EDGES) {
             this.isActive = true;
         }
         else {
@@ -55,18 +59,18 @@ export class Interactable extends Objects{
 
 
     // SETTERS
-    onInteract(){
-        if (this.player.currentPoints >= this.price && this.isActive) {
-            if (this.isPaidOnlyOnce) {
-                this.price = 0;
-                this.isPaidOnlyOnce = false;
-            }
+    // onInteract(){
+    //     if (this.player.currentPoints >= this.price && this.isActive) {
+    //         if (this.isPaidOnlyOnce) {
+    //             this.price = 0;
+    //             this.isPaidOnlyOnce = false;
+    //         }
 
-            this.player.points -= this.price;
-            this.onUse();
-        }
-        else {
-            console.log("Not enough points!") //todo: make this a popup text
-        }
-    };
+    //         this.player.points -= this.price;
+    //         this.onUse();
+    //     }
+    //     else {
+    //         console.log("Not enough points!") //todo: make this a popup text
+    //     }
+    // };
 };
